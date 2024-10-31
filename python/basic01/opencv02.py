e35ded1e3c4bd6b19fe953c232ed092455fe7f37
Hugo MG Sung <personar95@naver.com>
2024-10-31 11:11:16 +0900

    영어는?

--- python/basic01/opencv02.py
+++ python/basic01/opencv02.py
@@ -7,10 +7,11 @@ result = np.zeros((image.shape[0], 256), dtype=np.uint8)
 hist = cv2.calcHist([image], [0], None, [256], [0, 256])
 cv2.normalize(hist, hist, 0, 256, cv2.NORM_MINMAX)
 
-for	x, y in enumerate(hist):
-    cv2.line(result, (x, image.shape[0]), (x, image.shape[0]-y), 255)
+# 아래에서 오류 생김. 확인할 것!!!
+# for	x, y in enumerate(hist):
+#     cv2.line(result, (x, image.shape[0]), (x, image.shape[0]-y), 255)
     
 dst = np.hstack([image[:, :, 0], result])
-cv2.imshow("dst", image)
+cv2.imshow("dst", dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
