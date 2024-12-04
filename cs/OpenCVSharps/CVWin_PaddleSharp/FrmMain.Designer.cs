7f0b562194ae67938e8780b910b1f9f5a9518b09
Hugo MG Sung <personar95@naver.com>
2024-12-04 18:47:32 +0900

    PaddleOCR C# 제대로 안됨

---
+++ cs/OpenCVSharps/CVWin_PaddleSharp/FrmMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CVWin_PaddleSharp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PicResult = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)PicResult).BeginInit();
+            SuspendLayout();
+            // 
+            // PicResult
+            // 
+            PicResult.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            PicResult.Location = new Point(12, 12);
+            PicResult.Name = "PicResult";
+            PicResult.Size = new Size(1256, 669);
+            PicResult.TabIndex = 2;
+            PicResult.TabStop = false;
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1280, 693);
+            Controls.Add(PicResult);
+            Name = "FrmMain";
+            Text = "OCR PaddleSharp Example";
+            Load += FrmMain_Load;
+            ((System.ComponentModel.ISupportInitialize)PicResult).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox PicResult;
+    }
+}
---
+++ cs/OpenCVSharps/CVWin_PaddleSharp/FrmMain.cs
@@ -0,0 +1,62 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Text;
+using Sdcb.PaddleInference;
+using Sdcb.PaddleOCR.Models.Online;
+using Sdcb.PaddleOCR.Models;
+using Sdcb.PaddleOCR;
+using System.Security.Cryptography;
+
+namespace CVWin_PaddleSharp
+{
+    public partial class FrmMain : Form
+    {
+        private string strResult = string.Empty;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void FrmMain_Load(object sender, EventArgs e)
+        {
+            FullOcrModel model = await OnlineFullModels.EnglishV3.DownloadAsync();
+
+            byte[] sampleImageData;
+            string sampleImageUrl = @"http://sanhaesam2020.mysam98.gethompy.com/files/attach/images/157/247/a4c5e6faf95ac81ee52c0c53644f6fe5.jpg";
+            using (HttpClient http = new HttpClient())
+            {
+                Console.WriteLine("Download sample image from: " + sampleImageUrl);
+                sampleImageData = await http.GetByteArrayAsync(sampleImageUrl);
+            }
+
+            using (PaddleOcrAll all = new PaddleOcrAll(model, PaddleDevice.Mkldnn())
+            {
+                AllowRotateDetection = true, /* 기울어진 텍스트 인식 가능 */
+                Enable180Classification = false, /* 0도 이상의 회전 각도로 텍스트를 인식 */
+            })
+            {
+                // Load local file by following code:
+                // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
+                using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
+                {
+                    // 마지막에 윈도우 화면에 뿌리기!!
+                    this.PicResult.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(src);
+
+
+                    PaddleOcrResult result = all.Run(src);
+                    Console.WriteLine("Detected all texts: \n" + result.Text);
+                    foreach (PaddleOcrResultRegion region in result.Regions)
+                    {
+                        strResult = $"Text: {region.Text}, Score: {region.Score}, RectCenter: {region.Rect.Center}, RectSize:    {region.Rect.Size}, Angle: {region.Rect.Angle}";
+
+                        ProcResultMessage(strResult);
+                    }
+                }
+            }
+        }
+
+        private void ProcResultMessage(string strResult)
+        {
+            MessageBox.Show(strResult);
+        }
+    }
+}
